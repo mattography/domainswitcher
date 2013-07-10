d3b1a9635f63ffd29963138b2767c3a5eff587f5
mattography <mattography@gmail.com>
2013-07-10 15:57:23 -0400

    Basic setup
    Basic implementation of a domain switcher

---
+++ domainchecker.js
@@ -0,0 +1,11 @@
+var trackID,
+    domain = window.location.hostname,
+    tID = ["UA-XXXXXXXX-1", "UA-XXXXXXXX-1", "UA-XXXXXXXX-2"]; //DEV, QA, APP
+
+  if(domain == "dev.url.com")               //if domain is dev.url.com
+    	trackID = tID[0];                     //track ID at array location 0
+	else if(domain == "qa.url.com")  				  //if domain is qa.url.com
+    	trackID = tID[1];            					//track ID at array location 1
+	else if(domain == "app.url.com")     			//if domain is app.url.com
+    	trackID = tID[2];									    //track ID at array location 2
+	var _gaq=[['_setAccount', trackID],['_trackPageview']];
